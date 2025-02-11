9d2db95a71ebb6c468ce9ead915510acca77b249
Sakamochanq <Sakamochan.q@outlook.jp>
2025-02-12 02:31:17 +0900

    feat: 顔の誤検知を防ぐパラメータ

--- src/Censor-Me/Detect.cs
+++ src/Censor-Me/Detect.cs
@@ -15,7 +15,12 @@ namespace Censor_Me
             using (var cascade = new CascadeClassifier("./assets/haarcascade_frontalface_default.xml"))
             {
                 // 顔認識
-                var faces = cascade.DetectMultiScale(mat);
+                var faces = cascade.DetectMultiScale(
+                    mat,
+                    scaleFactor: 1.2,
+                    minNeighbors: 6,
+                    minSize: new OpenCvSharp.Size(50, 50)
+                );
 
                 // 顔に四角を描画
                 foreach (var rect in faces)
